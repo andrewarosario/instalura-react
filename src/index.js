4d331d56c0fd32bd00d73f667c4db147885c5b1f
João Pedro Benedetti Misturini <joao.benedetti.misturini@gmail.com>
2018-02-24 23:50:58 -0300

    adiciona camada estática de login

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import './css/reset.css';
 import './css/timeline.css';
+import './css/login.css';
 import App from './App';
+import Login from './components/Login';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const is_logged = true;
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" render={props => is_logged ? <App {...props} /> : <Redirect to="/login" /> } />
+            <Route path="/login" render={props => !is_logged ? <Login {...props} /> : <Redirect to="/" /> } />
+        </Switch>
+    </BrowserRouter>
+    , document.getElementById('root'));
 registerServiceWorker();
